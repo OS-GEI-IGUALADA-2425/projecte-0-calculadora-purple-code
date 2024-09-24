e886eb26720ebcfe3d84715cbbc227b355f7182b
IzanJR14 <izanjr14@debianizan.local>
2024-09-24 13:14:01 +0200

    @feat: Implementació de la funció de resta

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+float resta(int a, int b) {
+    return a - b;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
+float resta(int a, int b);
 
 #endif // CALC_H
